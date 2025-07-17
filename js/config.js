59d61440cc5ab407c5767f8bce314ab43fda8660
Mubarek-Bora <mubhoney@yahoo.com>
2025-07-17 07:54:15 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_zOqhqUijy', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: ' 1jj3t1a29ibjsstc06n4kqnhv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'us-west-2_zOqhqUijy', // Removed space and line break
+        userPoolClientId: '1jj3t1a29ibjsstc06n4kqnhv', // Removed trailing space
+        region: 'us-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
